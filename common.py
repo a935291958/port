3da353f4879afd203a34f81b28d980b1d6a15c40
a935291958 <935291958@qq.com>
2018-05-07 14:35:07 +0800

    Initial commit

---
+++ common.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import os, re, shutil, time
+
+import chardet
+
+# 创建日志目录
+if not os.path.isdir('./log'):
+    os.mkdir('./log')
+
+
+# 写入日志
+def msg(str, type):
+    if not str or not type:
+        print '日志相关异常'
+        return False
+
+    msg = ''
+    pMsg = ''
+
+    # 打开日志文件
+    f = './log/' + time.strftime("%Y-%m-%d", time.localtime()) + '.log'
+    fLog = open(f, 'a')
+
+    # 拼接提示信息
+    msg = time.strftime("%H:%M:%S", time.localtime()) + '] ' + str
+
+    if (type == 1):
+        msg = '[INFO] [' + msg
+        pMsg = '\033[0;32;40m ' + msg
+
+    elif type == 2:
+        msg = '[WARNING] [' + msg
+        pMsg = '\033[0;33;40m ' + msg
+
+    elif type == 3:
+        msg = '[ALERT] [' + msg
+        pMsg = '\033[0;35;40m ' + msg
+
+    elif type == 4:
+        msg = '[ERROR] [' + msg
+        pMsg = '\033[0;31;40m ' + msg
+
+    elif type == 5:
+        msg = '[SUCCESS] [' + msg
+        pMsg = '\033[0;37;40m ' + msg
+    else:
+        print  'type参数错误!'
+        fLog.close()
+        return False
+
+    print pMsg
+
+    fLog.write(msg + '\n')
+    fLog.close()
+
+    pass
+
+
+# 判断是不是文件
+def isFile(filename):
+    return os.path.isfile(filename)
+
+
+# 判断是不是目录
+def isDir(dir):
+    return os.path.isdir(dir)
+
+
+# 递归删除目录，非空的也可以删除
+def remove_dir(dir):
+    dir = dir.replace('\\', '/')
+    if (os.path.isdir(dir)):
+        for p in os.listdir(dir):
+            remove_dir(os.path.join(dir, p))
+        if (os.path.exists(dir)):
+            os.rmdir(dir)
+    else:
+        if (os.path.exists(dir)):
+            os.remove(dir)
+    pass
+
+
+# 获取文件编码
+def getDetect(filename):
+    if not isFile(filename):
+        return False
+    f = open(filename, 'r')
+    fileData = f.read()
+    f.close()
+    return chardet.detect(fileData)
+
+
+# 设置文本为utf-8
+def setUtf8(str):
+    return str.decode('gbk').encode('utf-8')
+
+
+# 设置文本为gbk
+def setGbk(str):
+    return str.decode('utf-8').encode('gbk')
+
+
+# 设置文件编码
+def setCode(old, new):
+    return str.decode(old).encode(new)
+
+
---
+++ setup.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+
+import socket, os, sys, thread, time, Queue, threading, numpy as np
+from common import *
+
+if __name__ != '__main__':
+    sys.exit(1)
+
+# url文件
+urlFile = "NTP_IP.csv"
+
+
+# urlFile = "t.txt"
+
+# ************************************************#
+
+def isOpen(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, int(port)))
+        s.shutdown(2)
+        msg(ip + ' is open on ' + str(port), 1)
+        fos = open('success.txt', 'a')
+        fos.write(ip + '\r')
+        fos.close()
+        return True
+    except:
+        msg(ip + ' is down on ' + str(port), 2)
+        fos = open('fail.txt', 'a')
+        fos.write(ip + '\r')
+        fos.close()
+        return False
+
+
+# 打开文件
+fo = open(urlFile, "r")
+msg('file name is %s' % (urlFile), 1)
+ipList = []
+
+for line in fo.readlines():  # 依次读取每行
+    line = line.strip()  # 去掉每行头尾空白
+    line = line.replace('\n', '')  # 去掉每行换行符
+    # print line
+    # 空行的话跳过
+    if (not line):
+        continue
+    ipList.append(line)
+
+exitFlag = 0
+
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q
+
+    def run(self):
+        print "Starting " + self.name
+        process_data(self.name, self.q)
+        print "Exiting " + self.name
+
+
+def process_data(threadName, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print "%s processing %s" % (threadName, data)
+            isOpen(data, 123)
+        else:
+            queueLock.release()
+        time.sleep(1)
+
+
+threadList = np.arange(0, 100)
+nameList = ipList
+queueLock = threading.Lock()
+workQueue = Queue.Queue(0)
+threads = []
+threadID = 1
+
+# 创建新线程
+for tName in threadList:
+    thread = myThread(threadID, tName, workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
+
+# 填充队列
+queueLock.acquire()
+for word in nameList:
+    workQueue.put(word)
+queueLock.release()
+
+# 等待队列清空
+while not workQueue.empty():
+    pass
+
+# 通知线程是时候退出
+exitFlag = 1
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
+print "Exiting Main Thread"
